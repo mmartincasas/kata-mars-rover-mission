b0ec0448e59d57e1e07855df279f1470ae5a8706
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2025-04-05 14:22:04 +0200

    Implemented rover movement according to commands

--- src/components/RoverMap.vue
+++ src/components/RoverMap.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col">
-    <div
-      v-for="(row, rowIndex) in map.grid"
-      :key="rowIndex"
-      class="flex"
+    <div 
+    v-for="(row, rowIndex) in map.grid" 
+    :key="rowIndex" 
+    class="flex"
     >
       <div
         v-for="cell in row"
@@ -17,33 +17,34 @@
     </div>
   </div>
 </template>
-  
+
 <script lang="ts" setup>
 
-  import { reactive, ref, watch } from 'vue'
-  import type { CommandInput } from '@/models/CommandInput'
-  import { MAX_MAP_VALUE } from '@/constants/map'
-  import { createEmptyMap } from '@/utils/mapFactory'
+import { reactive, ref, watch } from 'vue'
+import type { CommandInput } from '@/models/CommandInput'
+import { MAX_MAP_VALUE } from '@/constants/map'
+import { createEmptyMap } from '@/utils/mapFactory'
 
-  const props = defineProps<{
-    commandInput: CommandInput | null
-  }>()
+const props = defineProps<{
+  commandInput: CommandInput | null
+}>()
 
-  const rover = reactive<CommandInput>({
-    x: 0,
-    y: 0,
-    direction: 'N',
-    commands: ''
-  })
+const rover = reactive<CommandInput>({
+  x: 0,
+  y: 0,
+  direction: 'N',
+  commands: ''
+})
 
-  const map = ref(createEmptyMap(MAX_MAP_VALUE))
+const map = ref(createEmptyMap(MAX_MAP_VALUE))
 
-  watch(
+watch(
   () => props.commandInput,
   (newCommands) => {
     if (newCommands) {
       getNewCommands(newCommands)
       drawRoverPosition()
+      moveRover()
     }
   }
 )
@@ -56,15 +57,85 @@ function getNewCommands (newCommands: CommandInput) {
   console.log('Rover received new commands:', rover)
 }
 
-function drawRoverPosition () {
+function moveRover() {
+  let index = 0;
 
-  map.value.grid.forEach(row => row.forEach(cell => cell.hasRover = false));
+  function executeNextMove() {
+    if (index < rover.commands.length) {
+      const command = rover.commands[index];
+
+      if (command === 'L' || command === 'R') {
+        turnRover(command);
+      }
+      switch (rover.direction) {
+        case 'N':
+          rover.y -= 1;
+          break;
+        case 'S':
+          rover.y += 1;
+          break;
+        case 'E':
+          rover.x += 1;
+          break;
+        case 'W':
+          rover.x -= 1;
+          break;
+      }
+
+      drawRoverPosition();
+
+      index++;
+
+      setTimeout(executeNextMove, 500);
+    }
+  }
+
+  executeNextMove();
+}
+
+
+function turnRover(direction: 'L' | 'R') {
+  if (direction === 'L') {
+    switch (rover.direction) {
+      case 'N':
+        rover.direction = 'W'
+        break
+      case 'S':
+        rover.direction = 'E'
+        break
+      case 'E':
+        rover.direction = 'N'
+        break
+      case 'W':
+        rover.direction = 'S'
+        break
+    }
+  } else if (direction === 'R') {
+    switch (rover.direction) {
+      case 'N':
+        rover.direction = 'E'
+        break
+      case 'S':
+        rover.direction = 'W'
+        break
+      case 'E':
+        rover.direction = 'S'
+        break
+      case 'W':
+        rover.direction = 'N'
+        break
+    }
+  }
+}
+
+function drawRoverPosition() {
+  map.value.grid.forEach ((row) => row.forEach((cell) => (cell.hasRover = false)))
 
   const cell = map.value.grid[rover.y]?.[rover.x]
   if (cell) {
     cell.hasRover = true
   }
-
+  
 }
 
 </script>
